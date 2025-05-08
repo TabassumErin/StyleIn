8784193d47f8422356f5ed210fd68a66295df7d8
TabassumErin <tabassumerin971@gmail.com>
2025-05-09 04:06:54 +0600

    Delete StyleIn/homepage.js

--- StyleIn/homepage.js
+++
@@ -1,8 +0,0 @@
-function scrollCarousel(direction) {
-    const carousel = document.getElementById('carousel');
-    const scrollAmount = 220;
-    carousel.scrollBy({
-      left: direction * scrollAmount,
-      behavior: 'smooth'
-    });
-  }
