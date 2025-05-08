00e0711d23aff53831d9d3dae5bb9914c160059d
TabassumErin <tabassumerin971@gmail.com>
2025-05-09 01:22:09 +0600

    Add files via upload

---
+++ StyleIn/homepage.js
@@ -0,0 +1,8 @@
+function scrollCarousel(direction) {
+    const carousel = document.getElementById('carousel');
+    const scrollAmount = 220;
+    carousel.scrollBy({
+      left: direction * scrollAmount,
+      behavior: 'smooth'
+    });
+  }
